e2c9837d79663be30cd482c7eca44cb565ee3ae1
Patrick Gray <pgrayobx@gmail.com>
2015-04-30 17:13:13 -0700

    robustified test cases and included notes in the test. detailed usage and assumptions and summary in the readme. made function and class names more clear in anagram_creator

---
+++ README.md
@@ -0,0 +1,43 @@
+###Agar Man: Anagram Finder
+
+####Summary:
+This program will:
+
+1.  Ingest a "dictionary" text file with one word per line
+
+2.  Output a list of anagrams contained in the input file if:
+
+  a.  (# of letters in the word) >= 4
+
+  b.  (# of anagrams in the group) >= (# of letters in the word)
+
+####Usage:
+
+```
+./anagram_finder.py --filename \<filename\> 
+```
+
+This assumes python 2.7 or greater is installed on the system.
+
+###Testing:
+
+For the included test case ```test_input.txt```
+
+Output should be:
+```
+hello, heoll, llheo, lhleo, olleh
+ascot, coast, costa, tacso, tasco
+emir, mire, reim, riem, rime
+yaas, aasy, saay, ayas
+```
+
+####Assumptions:
+
+-assuming that all words are in the dictionary - does not find external anagrams
+
+-assuming that output order doesn't matter - neither within anagram groups nor for the groups of anagrams themselves
+
+-assuming no repeated words
+
+####Potential Optimizations:
+-use an list [0-25] as the key value so I can use bucket sort instead of default python Timsort
--- anagram_creator.py
+++ anagram_creator.py
@@ -1,40 +1,35 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python
 
 '''
 Created on Apr, 29 2015
 
 @author: Patrick Gray
-
-@summary: Ingest a dictionary text file with one word per line and output a list of anagrams where the
-word has >= 4 letters and # of anagrams >= # of letters.
-
-potential optimizations:
--use an list [0-25] as the key value so I can use bucket sort instead of 
 '''
 
 import argparse
 
-class AnagramBuilder(object):
+class AnagramFinder(object):
     def __init__(self):
         self.anagram_dict = dict()
         pass
 
     def find_anagrams(self, dictionary_file_name):
         with open(dictionary_file_name) as dictionary_list:
+            #run through every word in the dictionary where word length is > 4
             for word in dictionary_list:
+                word = word.rstrip('\n')                
                 if len(word) >= 4:
-                    word = word.rstrip('\n')
-                    #run through every item len > 4 and organize the letters in alphabetical order
-                    #then add that item to a hash with a list of items with that same set of characters
-
+                    #sort characters in the word to have a consistent way to compare if words are anagrams
                     sorted_character_list = ''.join(sorted(word))
-                    
+
+                    #add that word to a hash with the value being a list of words that are anagrams and the key being the alphabetic sorted characters
                     if sorted_character_list in self.anagram_dict:
                         self.anagram_dict[sorted_character_list].append(word)
                     else:
                         self.anagram_dict[sorted_character_list] = [word]
-                
-        #then after reaching the last character iterate through the hash (dict) and print all groups with >= 4 words
+
+    def print_anagrams(self):
+        #iterate through the hash (dict) and print all groups with where # of words >= # of letters in each word
         for key, anagram_list in self.anagram_dict.items():
             if len(anagram_list) >= len(key):
                 print(', '.join(anagram_list))
@@ -50,9 +45,10 @@ def parse_args():
 def main(argv=None):
     filename = parse_args()
     
-    anagram_builder = AnagramBuilder()
+    anagram_finder = AnagramFinder()
 
-    anagram_builder.find_anagrams(filename)
+    anagram_finder.find_anagrams(filename)
+    anagram_finder.print_anagrams()
 
 if __name__ == "__main__":
     main()
